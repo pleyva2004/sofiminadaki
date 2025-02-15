e9a577715f43fff369e4b0571dbdb420ebea1610
Pablo <pleyva2004@gmail.com>
2025-02-15 08:50:13 -0500

    Fixing Last Name

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Sofia Mindaki <3').split('')
+    const titles = ('Sofia Minadaki <3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
